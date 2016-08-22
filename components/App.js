2273e1f1d0f99dae2082bcfe8963264c0ff190e6
pthiruna86 <pthirunavukkarasu@prosper.com>
2016-08-21 20:09:08 -0700

    Learnt webpack config, React components with an input text box eg

--- components/App.js
+++ components/App.js
@@ -1,1 +1,16 @@
+import React, { Component } from 'react'
+import TextInput from './TextInput'
 
+class App extends Component {
+
+	render() {
+		return (
+			<div> 
+				<h1>This is the App Component</h1>
+				<TextInput/>
+			</div>
+			)
+		}
+	}
+
+export default App
